985f2a12cbbe4ca2e03427d1ce4a4d2114b0197d
ylws <474569696@qq.com>
2017-09-12 17:20:23 +0800

    update localvideo

--- video/index.js
+++ video/index.js
@@ -1,6 +1,6 @@
-import {CFCMVideo} from './src/videos.babel.min.js'
+import {LocalVideo} from './src/videos.babel.min.js'
 export default {
  	install: function(Vue) {
-	 Object.defineProperty(Vue.prototype, '$Video', { value: CFCMVideo });
+	 Object.defineProperty(Vue.prototype, '$Video', { value: LocalVideo });
   }
 }
